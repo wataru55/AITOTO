8afec0032a262cdb4bbfbf2db9f0a158e824f1ad
Wataru Takahashi <wataru1355@icloud.com>
2024-07-19 12:48:10 +0900

    Initial Commit

---
+++ AITOTO/AITOTOApp.swift
@@ -0,0 +1,17 @@
+//
+//  AITOTOApp.swift
+//  AITOTO
+//
+//  Created by  髙橋和 on 2024/07/19.
+//
+
+import SwiftUI
+
+@main
+struct AITOTOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
